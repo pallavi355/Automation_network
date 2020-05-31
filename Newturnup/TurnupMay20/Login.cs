d30671d85aa841f4c20ac63884cb23667a1b989b
Sumit <erbindal@gmail.com>
2020-05-31 17:08:28 +1000

    new turn up added

---
+++ Newturnup/TurnupMay20/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace TurnupMay20
+{
+    public class Login
+    {
+        private IWebDriver driver; 
+             
+        public Login(IWebDriver driver)
+        {
+            this.driver = driver; 
+        }
+        
+        IWebElement UserName => driver.FindElement(By.Id("UserName"));
+        IWebElement Password => driver.FindElement(By.Name("Password"));
+        IWebElement LoginBtn => driver.FindElement(By.XPath("//input[@type='submit']"));
+
+
+        public void LoginSuccess()
+        {            
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+
+            // enter hari as username
+            UserName.SendKeys("hari");
+            //identfying password & sending password
+            Password.SendKeys("123123");
+            //clicked login btn
+            LoginBtn.Click();
+        }
+
+        public void LoginFailure()
+        {
+            //Identify username
+            // enter hari as username
+            UserName.SendKeys("hari");
+
+            //identfying password & sending password
+            Password.SendKeys("123123");
+        }
+    }
+}
---
+++ Newturnup/TurnupMay20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TurnupMay20
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // open a browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Creating instance of Login Page
+            var loginPage = new Login(driver);
+            loginPage.LoginSuccess();
+
+            // Creating an instance of home page
+            var homePage = new Home(driver);
+            homePage.clickAdminstration();
+            homePage.clickTimenMaterials();
+
+
+            TimeMaterial timeMaterial = new TimeMaterial(driver);
+            //timeMaterial.clickCreateNew();
+            //timeMaterial.CreateNewRecord();
+            //timeMaterial.EditNewRecord();
+            timeMaterial.DeleteNewRecord();
+            //timeMaterial.ValidateRecord();      
+
+            driver.Quit();
+
+        }
+    }
+}
